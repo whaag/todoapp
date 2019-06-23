d0c83ba253e90330dc1ffeaa8dbd3a6f9169a586
Eduardo Athayde <eduardo.a.athayde@gmail.com>
2019-06-23 10:43:23 +0100

    vue crash course

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0. It is part of the Vue crash course on <a href="https://www.youtube.com/watch?v=Wy9q22isx3U">Youtube</a></p>
+    <p>Using:</p> 
+    <p>https://jsonplaceholder.typicode.com/ axion and vue-router</p>
+  </div>
+</template>
